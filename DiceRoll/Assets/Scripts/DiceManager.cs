70fae0460f28a17a18b8cb0aa400e74b5b2179f7
Paul Baker <paulbaker_tech@shaw.ca>
2019-11-09 21:20:22 -0800

    Added dice manager, canvas, and buttons.

---
+++ DiceRoll/Assets/Scripts/DiceManager.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DiceManager : MonoBehaviour
+{
+    public DieController[] Dice;
+
+    private bool WaitingForNewScore;
+
+    public Text ScoreText;
+
+    private int currentScore = 0;
+
+    private void Start()
+    {
+        this.WaitingForNewScore = false;
+        this.UpdateScore();
+    }
+
+    private void Update()
+    {
+        if (this.WaitingForNewScore)
+        {
+            if (this.CheckIfAllStopped())
+            {
+                int newScore = this.GetTotalScore();
+                this.currentScore += newScore;
+                this.UpdateScore();
+                this.WaitingForNewScore = false;
+                if (newScore == 2)
+                {
+                    Debug.Log("GAME OVER!");
+                }
+            }
+        }
+    }
+
+    private bool CheckIfAllStopped()
+    {
+        bool allStopped = true;
+        foreach (var die in this.Dice)
+        {
+            if (die.DieStatus != DieStatus.Stopped)
+            {
+                allStopped = false;
+                break;
+            }
+        }
+        return allStopped;
+    }
+
+    public void Roll()
+    {
+        if (!this.WaitingForNewScore)
+        {
+            this.Reset();
+            this.WaitingForNewScore = true;
+            foreach (var die in this.Dice)
+            {
+                die.Roll();
+            }
+        }
+    }
+
+    public void Reset()
+    {
+        foreach (var die in this.Dice)
+        {
+            die.Reset();
+        }
+        this.WaitingForNewScore = false;
+    }
+
+    public int GetTotalScore()
+    {
+        int total = 0;
+        foreach (var die in this.Dice)
+        {
+            total += die.CurrentSide;
+        }
+        return total;
+    }
+
+    private void UpdateScore()
+    {
+        this.ScoreText.text = string.Format("SCORE: {0}", this.currentScore);
+    }
+}
---
+++ DiceRoll/Assets/Scripts/DieController.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum DieStatus
+{
+    Ready,
+    StartRoll,
+    Rolling,
+    Stopped
+}
+
+
+public class DieController : MonoBehaviour
+{
+    // Keep track of the current side;
+    public int CurrentSide { private set; get; } = 0;
+
+    private Vector3 startPosition;
+
+    private Rigidbody rigidBody;
+
+    public DieStatus DieStatus { private set; get; }
+
+    // Assign an outward pointing vector to each side.
+    private List<(int, Vector3)> sideList = new List<(int, Vector3)>
+    {
+        (1, Vector3.down),
+        (2, Vector3.forward),
+        (3, Vector3.up),
+        (4, Vector3.back),
+        (5, Vector3.right),
+        (6, Vector3.left)
+    };
+
+    void Awake()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    void Start()
+    {
+        this.startPosition = this.transform.position;
+        this.DieStatus = DieStatus.Ready;
+    }
+
+    void Update()
+    {
+        if (this.DieStatus == DieStatus.StartRoll)
+        {
+            this.DieStatus = DieStatus.Rolling;
+        }
+        else if (this.DieStatus == DieStatus.Rolling)
+        {
+            float maxValue = -1.0f;
+            int maxSideIndex = -1;
+            for (int i = 0; i < this.sideList.Count; i++)
+            {
+                // Go through each side and determine which side is pointing up.
+                float dotValue = Vector3.Dot(Vector3.up, this.transform.TransformDirection(this.sideList[i].Item2));
+                if (dotValue > maxValue)
+                {
+                    maxValue = dotValue;
+                    maxSideIndex = i;
+                }
+            }
+            if (maxSideIndex >= 0)
+            {
+                this.CurrentSide = this.sideList[maxSideIndex].Item1;
+            }
+            if (this.IsRolling() == false)
+            {
+                this.DieStatus = DieStatus.Stopped;
+            }
+        }
+    }
+
+    private bool IsRolling()
+    {
+        bool rolling = true;
+        if (this.rigidBody.velocity.magnitude == 0f)
+        {
+            if (this.rigidBody.angularVelocity.magnitude == 0f)
+            {
+                rolling = false;
+            }
+        }
+        return rolling;
+    }
+
+    public void Roll()
+    {
+        this.rigidBody.AddForce(Vector3.up * Random.Range(0.5f, 1.5f), ForceMode.Impulse);
+        this.rigidBody.AddTorque(Vector3.left * Random.Range(1f, 50f), ForceMode.Impulse);
+        this.rigidBody.AddTorque(Vector3.forward * Random.Range(1f, 50f), ForceMode.Impulse);
+        this.rigidBody.AddTorque(Vector3.up * Random.Range(1f, 50f), ForceMode.Impulse);
+        this.DieStatus = DieStatus.StartRoll;
+    }
+
+    public void Reset()
+    {
+        this.rigidBody.velocity = new Vector3(0f, 0f, 0f);
+        this.rigidBody.angularVelocity = new Vector3(0f, 0f, 0f);
+        this.transform.position = this.startPosition;
+        this.transform.rotation = Quaternion.identity;
+        this.DieStatus = DieStatus.Ready;
+    }
+
+}
